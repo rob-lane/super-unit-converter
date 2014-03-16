a23e043ab64a36edf3f0fe47ae3f3265389dc108
roblane09 <roblane09@gmail.com>
2014-03-16 14:42:16 -0700

    unit map class now stores labels in alphabetical order and retrieves dictionary items in this order.

--- Super Unit Converter/RJUnitMap.h
+++ Super Unit Converter/RJUnitMap.h
@@ -11,9 +11,16 @@
 
 @interface RJUnitMap : NSObject  {
     NSMutableDictionary *unitDictionary;
+    NSMutableOrderedSet *unitLabels;
 }
+
+-(id) initWithContentsOfFile:(NSString*) path;
+
+-(RJUnit*) unitAtIndex:(unsigned int) index;
+-(NSString*) labelAtIndex:(unsigned int) index;
 
 -(RJUnit*) unitNamed:(NSString*) name;
 -(void) registerUnit:(RJUnit*) unit withName:(NSString*) name;
+-(NSArray*) unitTitles;
 
 @end
--- Super Unit Converter/RJUnitMap.m
+++ Super Unit Converter/RJUnitMap.m
@@ -8,6 +8,12 @@
 
 #import "RJUnitMap.h"
 
+@interface RJUnitMap ()
+
+-(void) _sortUnitLabels;
+
+@end
+
 @implementation RJUnitMap
 
 -(id) init {
@@ -15,9 +21,38 @@
     
     if (self) {
         unitDictionary = [[NSMutableDictionary alloc] init];
+        unitLabels = [[NSMutableOrderedSet alloc] init];
     }
     
     return self;
+}
+
+-(id) initWithContentsOfFile:(NSString *)path {
+    self = [super init];
+    
+    if (self) {
+        unitDictionary = [[NSMutableDictionary alloc] init];
+        NSArray *unitDefinitionSet = [NSArray arrayWithContentsOfFile:path];
+        for (NSDictionary *unitMapEntry in unitDefinitionSet) {
+            //Get ratio property for new property as a double
+            double ratio = ((NSNumber*)[unitMapEntry
+                                        objectForKey:@"ratio"]).doubleValue;
+            //create and register unit
+            RJUnit *newUnit = [[RJUnit alloc] initWithRatio:ratio];
+            [self registerUnit:newUnit withName:[unitMapEntry
+                                                 objectForKey:@"name"]];
+        }
+    }
+    
+    return self;
+}
+
+-(RJUnit*) unitAtIndex:(unsigned int)index {
+    return [self unitNamed:[self labelAtIndex:index]];
+}
+
+-(NSString*) labelAtIndex:(unsigned int)index {
+    return [unitLabels objectAtIndex:index];
 }
 
 -(RJUnit*) unitNamed:(NSString *)name {
@@ -26,6 +61,20 @@
 
 -(void) registerUnit:(RJUnit *)unit withName:(NSString *)name {
     [unitDictionary setObject:unit forKey:name];
+    if ( [unitLabels containsObject:name] == NO) {
+        [unitLabels addObject:name];
+        [self _sortUnitLabels];
+    }
+}
+
+-(NSArray*) unitTitles {
+    return [unitDictionary allKeys];
+}
+
+-(void) _sortUnitLabels {
+    [unitLabels sortUsingComparator: ^NSComparisonResult(id label1, id label2) {
+        return [label1 compare:label2];
+    }];
 }
 
 
